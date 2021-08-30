398c4a1c50de1b70fbc47f15f228f312f51f6f5b
DESKTOP-0KACA77\hp <priteshbhangale04@gmail.com>
2021-08-30 11:35:05 +0530

    [Pritesh] Entered Valid Username

--- src/com/bridgelabs/regex/regexClass.java
+++ src/com/bridgelabs/regex/regexClass.java
@@ -1,7 +1,17 @@
 package com.bridgelabs.regex;
 
 public class regexClass {
-public static void main(String[] args) {
-	
-}
+	public static void main(String[] args) {
+		String Uc1= "^[A-Z][a-zA-z]{2,}";
+		String name="Pri";
+		if(name.matches(Uc1))
+		{
+			System.out.println("ok");
+		}
+		else {
+			System.out.println("Not ok");
+		}
+
+		
+	}
 }
